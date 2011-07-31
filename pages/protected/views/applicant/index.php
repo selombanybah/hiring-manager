380970ceb5f3723a51edae0d53993b7e65023d83
shikhar.kr@gmail.com <shikhar.kr@gmail.com@138c7402-aa0b-d887-74a8-d49c23502ae0>
2011-07-31 07:11:32 +0000

    minor improvisation

--- pages/protected/views/applicant/index.php
+++ pages/protected/views/applicant/index.php
@@ -4,10 +4,7 @@ $this->breadcrumbs=array(
 	'Applicants',
 );
 
-$this->menu=array(
-	array('label'=>'Create Applicant', 'url'=>array('create')),
-	array('label'=>'Manage Applicant', 'url'=>array('admin')),
-);
+
 ?>
 
 <h1>Applicants</h1>
